81411aae22b0b8ab10f30c92c312bd296475d52f
a1barif2h <a1barif2h@gmail.com>
2020-09-14 02:30:26 +0600

    react router and material ui is install is complet

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const Header = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
+};
+
+export default Header;
